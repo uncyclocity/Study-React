51c256b1ec483f8c1fc40ee65bb0acaea9f28028
YSB_uncyclocity <seongbeom_lee@kakao.com>
2021-05-25 08:35:51 +0900

    [Update] App.js, UserList.js
    useRef를 이용하여 컴포넌트 안의 변수 만들기 실습 (in App 컴포넌트)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import UserList from './UserList'
-import './App.css'
+import {useRef} from 'react';
+import UserList from './UserList';
 
 function App() {
+  const users = [
+    {
+        id: 1,
+        username: 'velopert',
+        email: 'public.velopert@gmail.com'
+    },
+    {
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com'
+    },
+    {
+        id: 3,
+        username: 'liz',
+        email: 'liz@example.com'
+    }
+  ];
+
+  // useRef를 사용할 때 파라미터를 넣어 줄 경우, 파라미터 값이 .current의 기본 값이 된다.
+  // 값을 수정할 때는 .current 값을 수정, 조회 할 때도 .current 값을 조회하면 됨
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 배열 값 추가하는 로직을 여기에 입력
+    nextId.current += 1;
+  }
+
   return (
-    <UserList />
+    <UserList users={users}/>
   );
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -7,25 +7,7 @@ function User({user}) {
     )
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        }
-    ];
-
+function UserList({users}) {
     return (
         <div>
             {/* Array.prototype.map() : 각각의 요소를 순서대로 부름 (그리고 새로운 배열을 만듦)*/}
