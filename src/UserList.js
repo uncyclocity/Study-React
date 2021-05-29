19d7ffc4518c72905026b9e05546b6c82c025be8
YSB_uncyclocity <41094016+uncyclocity@users.noreply.github.com>
2021-05-30 01:53:40 +0900

    [Update] useEffect Hook 실습
    UserList.js의 User 컴포넌트에서 useEffect Hook을 실습하고, 이에 대한 내용을 각주로 정리함

--- src/UserList.js
+++ src/UserList.js
@@ -1,10 +1,34 @@
-import React from 'react';
-// import {useEffect} from 'react';
-/* useEffect는 재학습 필요 있음
-https://simsimjae.tistory.com/401 */
+import React, {useEffect, useRef} from 'react';
 
 // Array.prototype.map() 을 통해 호출되는 함수
 const User = React.memo(function User({user, onRemove, onToggle}) {
+    /* 
+    useEffect : 컴포넌트 랜더링 때마다 특정 작업 실행할 수 있도록 하는 Hook -> useEffect(function, deps)
+
+    1. Component가 mount 되었을 때 (나타났을 때)
+    - Component가 가장 처음 렌더링 되었을 때 한 번만 실행하려면, deps 위치에 빈 배열을 넣는다
+    - Component가 렌더링 될 때마다 실행하려면, deps 위치에 아무것도 안 넣으면 된다
+
+    2. Component가 업데이트 될 때 (특정 props, state가 바뀔 때)
+    - 특정 props, state 값이 변경되었을 때 실행하려면, deps 위치에 변화를 검사할 값을 집어넣으면 된다
+    - deps 값이 업데이트 될 때만 지정한 작업을 실행하려면, 아래의 예시를 참고하면 좋다
+
+    3. Component가 unmount 되었을 때 (사라질 때) & update 되기 직전에
+    - cleanup 함수가 반환되며, 이는 return 뒤에 나오는 함수를 의미한다.
+    - unmount 될 때에만 cleanup 함수를 실행하고 싶을 경우, deps 위치에 빈 배열을 넣는다
+    */
+
+    // 예시 : deps 값이 업데이트 될 때만 지정한 작업 실행
+    const isMounted = useRef(false);
+    useEffect(() => {
+        // deps 값이 있으므로 가장 첫 렌더링만 신경써주면 된다
+        if (!isMounted.current) {
+            isMounted.current = true;
+        } else {
+            console.log('deps 값이 업데이트됨');
+        }
+    },[user]);
+
     return(
         // jsx 외부에서 정의 된 event handler/callback은 파라미터를 넘겨줄 시 화살표 함수를 사용하여야 한다.
         <div>
