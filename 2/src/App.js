ec0b05bc7e1799d662190343ba0d7df1ae410968
YSB_uncyclocity <seongbeom_lee@kakao.com>
2021-06-04 11:41:17 +0900

    [Update] 챕터 2-1 sass 실습 진행(2)
    sass 실습을 진행하고 이를 정리함

--- 2/src/App.js
+++ 2/src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <div className="buttons">
-        <Button size="large">I LOVE REFRIGERATOR</Button>
+        <Button size="large" onClick={() => console.log("클릭됐다")}>
+          I LOVE REFRIGERATOR
+        </Button>
         <Button>I LOVE REFRIGERATOR</Button>
         <Button size="small">I LOVE REFRIGERATOR</Button>
       </div>
@@ -24,6 +26,28 @@ function App() {
         </Button>
         <Button color="pink">I LOVE REFRIGERATOR</Button>
         <Button size="small" color="pink">
+          I LOVE REFRIGERATOR
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" outline>
+          I LOVE REFRIGERATOR
+        </Button>
+        <Button color="gray" outline>
+          I LOVE REFRIGERATOR
+        </Button>
+        <Button size="small" color="pink" outline>
+          I LOVE REFRIGERATOR
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth>
+          I LOVE REFRIGERATOR
+        </Button>
+        <Button size="large" color="gray" fullWidth>
+          I LOVE REFRIGERATOR
+        </Button>
+        <Button size="large" color="pink" fullWidth>
           I LOVE REFRIGERATOR
         </Button>
       </div>
