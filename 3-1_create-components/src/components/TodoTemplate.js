ca39fe7fa9fe3da48095e92226439748334f51e8
YSB_uncyclocity <41094016+uncyclocity@users.noreply.github.com>
2021-07-04 22:29:12 +0900

    [CREATE] 3-1 CRA프로젝트 생성
    챕터 3-1 프로젝트를 생성하고 약간 진행함

---
+++ 3-1_create-components/src/components/TodoTemplate.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const TodoTemplateBlock = styled.div`
+  width: 512px;
+  height: 768px;
+
+  position: relative;
+  background: white;
+  border-radius: 16px;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+`;
+
+function TodoTemplate() {
+
+}
+
+export default TodoTemplate;
